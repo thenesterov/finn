619e0a6c216d7400340641cbd14f4ff9e40a74a6
thenesterov <thenesterov@users.noreply.github.com>
2025-06-26 07:25:55 +0000

    Deploying to gh-pages from @ thenesterov/finn@c6b05f810935fbbd6522285b21df5892593a6124 🚀

--- node.d.ts
+++ node.d.ts
@@ -2640,7 +2640,9 @@ declare namespace $ {
 declare namespace $ {
 
 	export class $mol_theme_auto extends $mol_plugin {
-		theme( ): string
+		dark( ): string
+		theme( ): ReturnType< $mol_theme_auto['dark'] >
+		light( ): string
 		attr( ): ({ 
 			'mol_theme': ReturnType< $mol_theme_auto['theme'] >,
 		}) 
@@ -2651,7 +2653,7 @@ declare namespace $ {
 //# sourceMappingURL=auto.view.tree.d.ts.map
 declare namespace $.$$ {
     class $mol_theme_auto extends $.$mol_theme_auto {
-        theme(): "$mol_theme_light" | "$mol_theme_dark";
+        theme(): string;
     }
 }
 
