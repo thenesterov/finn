cff8e49d19d3c57cc64291cca5c56e513ade180f
thenesterov <thenesterov@users.noreply.github.com>
2026-03-17 07:57:39 +0000

    Deploying to gh-pages from @ thenesterov/finn@a48778262602dfe6ff72f849e494de06e5cd25be 🚀

--- node.js
+++ node.js
@@ -7790,6 +7790,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
--- node.test.js
+++ node.test.js
@@ -7781,6 +7781,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
